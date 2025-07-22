8e06d77fbac8c57c5f807b544ff46c4a621d225f
Emeka <emekaugochukwu002@gmail.com>
2025-07-22 10:26:57 +0100

    Updated the models file

--- models.py
+++ models.py
@@ -1,4 +1,14 @@
-from sqlalchemy import 
-
+from sqlalchemy import Column, Integer, String, Float, Date
+from database import Base
 import datetime
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    category = Column(String, nullable=False)
+    description = Column(String)
+    date = Column(Date, default=datetime.date.today)
+
 
